498b9c11e900404101d5d08e6c2d484da67c291a
Rohit Patil <71687498+falconcode16@users.noreply.github.com>
2021-03-11 23:38:47 +0530

    Add files via upload

---
+++ 4thcat.2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int l,b;
+main()
+{
+int area;
+
+    area=mult();
+    printf("area=%d\n",area);
+}
+int mult()
+{
+     int a;
+
+    printf("enter the value of a and b=%f\n",l,b);
+scanf("%d%d",&l,&b);
+
+
+   a=l*b;
+
+return(a);
+
+}
